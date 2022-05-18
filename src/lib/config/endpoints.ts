78c3eab93b4c5f62811dbd3f4860964b1b577d35
Taekhun <nth9708@naver.com>
2022-05-18 23:39:20 +0900

    feat: 고정 상수

---
+++ src/lib/config/endpoints.ts
@@ -0,0 +1,6 @@
+export const API_ENDPOINT = `https://node.wingeat.com/test-api`;
+export const IMAGE_ENDPOINT = `https://image.wingeat.com`;
+
+export const FEATURES = `features`;
+export const ITEMS = `items`;
+export const LOGO_IMAGE = `logo/images/we_logo_center.png`;
