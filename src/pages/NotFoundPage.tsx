2ede8ea7fceffa364915d77cf18910f7ad600176
Taekhun <nth9708@naver.com>
2022-05-19 16:10:25 +0900

    feat: 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import mainLogoSrc from '@assets/logo/main_logo.png';
+import { useCallback } from 'react';
 
-type Props = {};
+interface Props {}
 
 const NotFoundPage = (props: Props) => {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+
+  const onClickButton = useCallback(() => {
+    navigate(-1);
+  }, [navigate]);
+
+  return (
+    <Wrapper>
+      <ImageWrapper>
+        <Logo src={mainLogoSrc} />
+      </ImageWrapper>
+      <Title>요청하신 페이지를 찾을 수 없습니다.</Title>
+      <BackButton onClick={onClickButton}>뒤로 가기</BackButton>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 3rem;
+  padding: 1.5rem;
+`;
+
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 1.5rem;
+`;
+
+const ImageWrapper = styled.div`
+  width: clamp(150px, 50vw, 180px);
+`;
+
+const Logo = styled.img``;
+
+const BackButton = styled.button`
+  background-color: black;
+  color: white;
+  padding: 0.5rem 0.8rem;
+  border-radius: 6px;
+  font-size: 1.2rem;
+  font-weight: 500;
+`;
 
 export default NotFoundPage;
