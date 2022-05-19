ab691d5a10b9090e0f398931cbe847311e37c461
Taekhun <nth9708@naver.com>
2022-05-19 15:39:56 +0900

    refactor: item list refactoring

--- src/components/MainPage/ItemList/index.tsx
+++ src/components/MainPage/ItemList/index.tsx
@@ -1,22 +1,33 @@
+import { IMAGE_ENDPOINT } from '@lib/config/endpoints';
+import { priceCommaRegex } from '@lib/utils';
 import { ItemType } from '@types';
-import React, { forwardRef } from 'react';
 import styled from 'styled-components';
-import ItemCard from './ItemCard';
 
 type Props = {
   itemList: ItemType[] | [];
-  lastElemRef?: (node: any) => void;
+  handleItemClick: (item: ItemType) => void;
+  lastElemRef: (node: HTMLImageElement) => void;
 };
 
-const ItemList = forwardRef(({ itemList }: Props, ref) => {
+const ItemList = ({ itemList, handleItemClick, lastElemRef }: Props) => {
   return (
     <List>
-      {itemList.map((item, key) => (
-        <ItemCard ref={ref} item={item} key={key} {...(key === itemList.length - 1 && { isLastItem: true })} />
+      {itemList?.map(({ id, image, itemName, price }, key) => (
+        <ItemCard onClick={() => handleItemClick({ id, image, itemName, price })} key={key}>
+          <Image
+            src={`${IMAGE_ENDPOINT}/${image}`}
+            data-id={id}
+            alt={`item-image-${id}`}
+            isLast={key === itemList.length - 1}
+            {...(key === itemList.length - 1 && { ref: lastElemRef })}
+          />
+          <ItemName>{itemName}</ItemName>
+          <Price>{priceCommaRegex(price)}</Price>
+        </ItemCard>
       ))}
     </List>
   );
-});
+};
 
 const List = styled.ul`
   display: grid;
@@ -24,12 +35,37 @@ const List = styled.ul`
   user-select: none;
   grid-template-columns: repeat(4, minmax(0, 1fr));
   justify-items: center;
+  margin-bottom: 5rem;
   gap: 2rem;
-  min-height: 100vh;
 
   @media ${({ theme }) => theme.size.mobile} {
     grid-template-columns: repeat(2, minmax(0, 1fr));
-    /* padding: 0 20px 30px; */
+  }
+`;
+
+export const ItemCard = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  cursor: pointer;
+`;
+
+export const Image = styled.img<{ isLast: boolean }>`
+  width: 100%;
+  border-radius: 0.5rem;
+  margin-bottom: 0.8rem;
+`;
+
+export const ItemName = styled.p`
+  margin-bottom: 0.8rem;
+`;
+
+export const Price = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+
+  &:after {
+    content: '원';
   }
 `;
 
