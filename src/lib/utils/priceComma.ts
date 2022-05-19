beb61847270307716165568f3e64c177ced324dd
Taekhun <nth9708@naver.com>
2022-05-19 15:46:13 +0900

    chore: 파일 정리

--- src/lib/utils/priceComma.ts
+++
@@ -1,6 +0,0 @@
-export const priceCommaRegex = (num: number): string => {
-  if (num === 0) return '';
-
-  const parsedStr = String(num).replace(/[^\d]+/g, '');
-  return parsedStr.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
-};
